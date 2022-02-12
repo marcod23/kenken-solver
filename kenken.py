81d874a44e06a22eef4b25f0fe6c152660997103
mdeleon190 <marco131737@gmail.com>
2022-02-12 08:27:04 -0800

    adding number group data

--- kenken.py
+++ kenken.py
@@ -1,4 +1,13 @@
-grid_size = 9
+grid_size = 4
+info1 = [['a1', 'b1'], ['a2', 'b2']]
+info2 = [5, 2]
+info3 = ['+', '/']
+data = list(zip(info1, info2, info3))
+print(data)
+print(data[1][2])
+print(data[1][0][1])
+print(type(data[0][1]))
+
 key_list = []
 value_list = [i+1 for i in range(grid_size)]
 
@@ -12,4 +21,4 @@ kenken_grid = dict.fromkeys(key_list, value_list)
 
 #print(kenken_grid)
 
-print(kenken_grid['d3'])
+#print(kenken_grid['d3'])
