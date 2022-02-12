94b900936437a4c6cb776ffd2ec3f30644fa4535
mdeleon190 <marco131737@gmail.com>
2022-02-12 08:07:59 -0800

    initialize the kenken grid

--- kenken.py
+++ kenken.py
@@ -1,5 +1,15 @@
-grid_size = 6
+grid_size = 9
 key_list = []
 value_list = [i+1 for i in range(grid_size)]
 
-print(value_list)
+alphabet = [chr(i) for i in range(ord('a'), ord('a') + grid_size)]
+for letter in alphabet:
+    for num in value_list:
+        square_label = letter + str(num)
+        key_list.append(square_label)
+
+kenken_grid = dict.fromkeys(key_list, value_list)
+
+#print(kenken_grid)
+
+print(kenken_grid['d3'])
